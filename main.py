7c50787d2ad267dc4bacd646d5703b5a619d6b67
Martin <1135485226@qq.com>
2019-05-01 20:58:31 +0800

    added zero-pad when training a resnet model

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ device = torch.device('cuda:0' if use_gpu else 'cpu')
 
 def get_dataset_dataloader(input_data_type, seg_type, batch_size):
     data_transforms = transforms.Compose([
+            ZeroPad(),
             NormalizeBRATS(),
             ToTensor()
         ])
@@ -130,8 +131,8 @@ def get_fcn_model(num_classes, use_gpu):
 def get_unet_model(input_channels, num_classes, use_gpu):
     # vgg_model = VGGEncoder(pretrained=True, requires_grad=True, remove_fc=True)
     # unet = UNetWithVGGEncoder(vgg_model, num_classes)
-    unet = UNet(input_channels, num_classes)
-    # unet = UNetWithResnet50Encoder(input_channels, num_classes)
+    # unet = UNet(input_channels, num_classes)
+    unet = UNetWithResnet50Encoder(input_channels, num_classes)
     if use_gpu:
         ts = time.time()
         unet = unet.to(device)
